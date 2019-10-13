b32c2cf547e0d51d107eef6d62643f3ee296a907
Tanja <tanja.heinrich@htwg-konstanz.de>
2019-10-13 17:01:23 +0200

    Update Message

--- GitExample/src/gitPrepare/First.java
+++ GitExample/src/gitPrepare/First.java
@@ -4,6 +4,7 @@ public class First {
 
 	public static void main(String[] args) {
 	System.out.println("First Class has been released. It is now searching for updates.");
+	System.out.println("Test second Branch");
 
 	}
 
