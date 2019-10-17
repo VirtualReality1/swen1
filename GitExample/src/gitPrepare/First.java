39bff6705f01cd29d8279ed9b3e3adaefe48529d
Tanja <tanja.heinrich@htwg-konstanz.de>
2019-10-17 09:15:21 +0200

    Commit_01

--- GitExample/src/gitPrepare/First.java
+++ GitExample/src/gitPrepare/First.java
@@ -4,7 +4,7 @@ public class First {
 
 	public static void main(String[] args) {
 		Patient p = new Patient();
-		p.addPatID("121212121");
+		p.addPatID("1212121211");
 
 		System.out.println("First Class has been released. It is now searching for updates.");
 		System.out.println("Test second Branch");
