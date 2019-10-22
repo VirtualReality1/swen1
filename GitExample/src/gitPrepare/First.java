ab4b3cdd47168efa8dccca0f7e4b5ed4b7df1743
Tanja <tanja.heinrich@htwg-konstanz.de>
2019-10-22 20:43:19 +0200

    Klasse Patient inital Version:

--- GitExample/src/gitPrepare/First.java
+++ GitExample/src/gitPrepare/First.java
@@ -3,19 +3,19 @@ package gitPrepare;
 public class First {
 
 	public static void main(String[] args) {
-		Patient p = new Patient();
-		p.addPatID("1212121211");
+		Patient p = new Patient("Peter", "Lustig");
+		p.addPatID("aA19847263");
 
-		System.out.println("First Class has been released. It is now searching for updates.");
-		System.out.println("Test second Branch");
+		//System.out.println("First Class has been released. It is now searching for updates.");
+		//System.out.println("Test second Branch");
 
-		System.out.println("First Class has been released.");
-		System.out.println("This Bug has been fixed!");
+		//System.out.println("First Class has been released.");
+		//System.out.println("This Bug has been fixed!");
 
-		System.out.println("A bug came up, go fix it4!");
+		//System.out.println("A bug came up, go fix it4!");
 
-		System.out.println("A bug came up, go fix it!!!");
-		System.out.println("Fixed bug!");
+		//System.out.println("A bug came up, go fix it!!!");
+		//System.out.println("Fixed bug!");
 
 	}
 
--- GitExample/src/gitPrepare/Patient.java
+++ GitExample/src/gitPrepare/Patient.java
@@ -1,19 +1,31 @@
 package gitPrepare;
 
+import java.util.regex.*;
+
+import org.omg.Messaging.SyncScopeHelper;
+
 public class Patient {
- private String PatID; // soll 10 Zeichen enthalten ersten zwei Ziffern sind Buchstaben
- private String PaFirstName;
- private String paLastName;
- 
-public String addPatID(String PatID) {
-	this.PatID = PatID;
-	if (PatID.length()!=10) {
-		throw new IllegalArgumentException("PatientenID muss die Länge 10 haben!");
+	private String PatID; // soll 10 Zeichen enthalten ersten zwei Ziffern sind Buchstaben
+	private String PaFirstName;
+	private String paLastName;
+	private boolean checkID;
+
+	public Patient(String PaFirstName, String PaLastName) {
+		this.PaFirstName = PaFirstName;
+		this.paLastName = PaLastName;
+		
 	}
-	System.out.println(PatID);
-	return PatID;
-	
-	
-}
- 
+
+	//Methode überprüft, ob eingegebene Patientenidentifikationsnummer dem Muster Buchstabe,Buchstabe,Ziffer 1-8 entspricht, wenn ja liefert sie checkID= true zurück und fügt Nummer hinzu.
+	public boolean addPatID(String patID) {
+		checkID = false;
+		if(Pattern.matches("[a-zA-Z]{2}[0-9]{8}", patID)) {
+			checkID = true;
+			this.PatID = patID;
+		} else {
+			throw new IllegalArgumentException("Patientenidentifikationsnummer muss aus 2 Buchstaben am Anfang gefolgt von 8 Zahlen bestehen.");
+		}
+		return checkID;
+	}
+
 }
