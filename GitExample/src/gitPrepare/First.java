8b976e6c883391065edb62adf8639bf5daed86b3
Tanja <tanja.heinrich@htwg-konstanz.de>
2019-10-22 21:38:20 +0200

    Deleted Line in First.class

--- GitExample/src/gitPrepare/First.java
+++ GitExample/src/gitPrepare/First.java
@@ -4,7 +4,6 @@ public class First {
 
 	public static void main(String[] args) {
 		Patient p = new Patient();
-		p.addPatID("1212121211");
 
 		System.out.println("First Class has been released. It is now searching for updates.");
 		System.out.println("Test second Branch");
